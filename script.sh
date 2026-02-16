2f8a83a10fb4284381911dbe2ff9af95fe01759f
dimka <dimonu4.1989@gmail.com>
2026-02-16 16:40:01 +0300

    wata

--- script.sh
+++ script.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 again=yes
-while [[ again=yes ]]
+
+while [[ $again = "yes" ]]
 do
+<<<<<<< HEAD
 	read num1 num2
 	if [[ $num1 -eq "" || $num2 -eq "" ]]
+=======
+        mathDo ()
+	{
+		res=$(($1 $2 $3))
+		echo $res
+	}
+
+	read in1 in2 in3
+	if [[ $in1 = exit && -z $in2 && -z $in3 ]]
+>>>>>>> d5a0979 ( newest)
 	then
-		echo bye
+		echo "bye"
 		break
 	fi
+<<<<<<< HEAD
 	gcd ()
 	{
 		if [[ $1 -eq $2 ]]
@@ -27,4 +40,13 @@ do
 	}
 	gcd $num1 $num2
 done
+=======
+	if [[ $in2 != + && $in2 != "-" && $in2 != "*" && $in2 != "/" && $in2 != "%" && $in2 != "**" ]]
+	then
+		echo "error"
+		break
+	fi
+	mathDo $in1 "$in2" $in3
+>>>>>>> d5a0979 ( newest)
 
+done
